69027bb0b92bd964f2dab114dcd035d5f96ed2b8
Pramuditha <pramudithamendis444@gmail.com>
2023-12-06 15:33:54 +0530

    Sorted out the issue with the Output mentioned in the before commit

--- src/designpatterns/Test.java
+++ src/designpatterns/Test.java
@@ -6,15 +6,19 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		MissileOperation lm = new LaunchMissile();	
+		MissileOperation bm = new BlastMissile();
 		MissileSystem hms = HeatMissileSystem.getInstance();
 		MissileController mc = MissileController.getInstance();
 		
 		
 		hms.setMO(lm);
+
 		mc.setMS(hms);
 
 		
 		mc.performLaunching("Colombo");
+		
+		hms.setMO(bm);
 		mc.performBlasting("Col2");
 		
 //		System.out.println("Hi");
