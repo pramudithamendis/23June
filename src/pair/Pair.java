ff97b3f268c6d42d760a3806c232255e01d318f6
Pramuditha <pramudithamendis444@gmail.com>
2023-12-06 14:59:46 +0530

    When putting to \"printFirstTwoNew, we must put based on the Wrappers that we gave inside the Main method for those Object creation point. But for those Object creation point ones, we can give anything noh \"

--- src/pair/Pair.java
+++ src/pair/Pair.java
@@ -1,42 +1,57 @@
 package pair;
 
-public class Pair<T,U> {
+public class Pair<T,U, W extends Number ,X,A> {
 	
-	T i;
-	U j;
+	T t;
+	U u;
+	W w;
+	X x;
+	A a;
 	
-	public Pair(T i, U j){
-		this.i = i;
-		this.j = j;
+	public Pair(T t, U u, W w, X x, A a){
+		this.t = t;
+		this.u = u;
+		this.w = w;
+		this.x = x;
+		this.a = a;
 	}
 	public T getFirst() {
 		 
 		
-		return i;
+		return t;
 		
 	}
 	
 	public U getSecond() {
 
-		return j;
+		return u;
 		
 	}
 	
 	
-	public <T,U> void printFirst(T t, U u) {
+	public void printFirstTwo() {
+		System.out.println(t+"-"+u);
+	}
+	
+
+	public void printOther3() {
+		System.out.println(w + "-"+ x + "-"+ a);
+	}
+
+	public <T,U> void printFirstTwoNew(T t, U u) {
 		
-		System.out.println("First stuff");
-		
+		System.out.println(t +" "+ u);
+	
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Creating an object from the Pair generic class
-		Pair<String, Integer> p = new Pair<>("Test 1", 42);
+		Pair<String, Integer, Integer, Integer, Integer> p = new Pair<>("Test 1", 42, 1,2,3);
 
 		//Creating an object from the Pair generic class
-		Pair<Double, String> p2 = new Pair<>(3.14, "Test 2");
+		Pair<Double, String, Integer, Integer, Integer> p2 = new Pair<>(3.14, "Test 2", 4,5,6);
 
 		
 		//Printing the first object
@@ -51,9 +66,18 @@ public class Pair<T,U> {
 		System.out.println("Second value: "+ p2.getSecond());
 		
 		
-		p.printFirst("Hi", 12);
 		
+		System.out.println("");
+		p.printFirstTwo();
+		p2.printFirstTwo();
+				
+		System.out.println("");
+		p.printOther3();
+		p2.printOther3();
 		
+		System.out.println("");
+		p.printFirstTwoNew("Hi", 12);
+		p2.printFirstTwoNew(1.2, "Hi");
 	}
 
 }
